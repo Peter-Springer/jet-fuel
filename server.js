3adf23fb61383cd42149f172714011fa43c179b8
Peter Springer <peterspringer829@gmail.com>
2016-11-28 15:04:08 -0700

    initial post and get requests set up

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'URL Shortener';
+app.locals.urls = {};
+
+app.get('/', (request, response) => {
+  response.send('visit /api/v1/urls/:id to view single url');
+});
+
+app.get('/api/v1/urls/:id', (request, response) => {
+  const { id } = request.params;
+  const url = app.locals.urls[id]
+
+  if (!url) { return response.sendStatus(404); }
+
+  response.json({ id, url });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+app.post('/api/v1/urls', (request, response) => {
+  const { url } = request.body;
+  const id = md5(url);
+
+  if (!url) {
+    return response.status(422).send({
+      error: 'No url property provided'
+    });
+  }
+
+  app.locals.urls[id] = url;
+
+  response.status(201).json({ id, url });
+});
