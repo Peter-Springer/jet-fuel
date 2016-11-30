5dbc3b15cbcf8793659949a261ce73329dbeb233
swanie21 <kswanie21@gmail.com>
2016-11-30 16:16:25 -0700

    toggle urls by date added

--- scripts.js
+++ scripts.js
@@ -1,28 +1,6 @@
-import $ from 'jquery'
-
-$('.submit').on('click', function(e) {
-  e.preventDefault();
-  const url = $('.url-component').val();
-
-  if (!validUrl(url)) {
-  return alert('Please enter a valid URL address');
-}
-
-  $.post("http://localhost:3000/api/v1/urls",
-    {
-      url: $('.url-component').val()
-    }).then(fetchAllUrls()).done(
-        function() {
-    console.log( "success!");
-  });
-  $('.url-component').val('');
-});
-
-function fetchAllUrls() {
-  $.get("http://localhost:3000/api/v1/urls")
-    .then(renderLinks)
-    .then(addLinksToPage);
-}
+'use strict';
+import $ from 'jquery';
+let count = 0;
 
 function renderLink(link) {
   return `
@@ -33,22 +11,62 @@ function renderLink(link) {
       <p>${link.date}</p>
       <p>Clicks: ${link.count}</p>
     </article>
-    `
+    `;
 }
 
 function renderLinks(links) {
   return links.map(renderLink);
 }
 
-function addLinksToPage(links) {
-  $("#urls").html(links);
+function reverseLinks(links) {
+  return links.map(renderLink).reverse();
 }
 
+function addLinksToPage(links) {
+  $('#urls').html(links);
+}
+
+function fetchAllUrls() {
+  $.get('http://localhost:3000/api/v1/urls')
+  .then(renderLinks)
+  .then(addLinksToPage);
+}
 
 function validUrl(url) {
   var urlTest = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
   return urlTest.test(url);
 }
+
+$('.submit').on('click', function(e) {
+  e.preventDefault();
+  const url = $('.url-component').val();
+
+  if (!validUrl(url)) {
+  return alert('Please enter a valid URL address');
+}
+
+  $.post('http://localhost:3000/api/v1/urls',
+    {
+      url: $('.url-component').val()
+    }).then(fetchAllUrls()).done(
+        function() {
+    console.log('success!');
+  });
+  $('.url-component').val('');
+});
+
+$('.sort').on('click', function() {
+  count +=1;
+  if (count % 2 !== 0) {
+    $.get('http://localhost:3000/api/v1/urls')
+    .then(reverseLinks)
+    .then(addLinksToPage);
+  } else {
+    $.get('http://localhost:3000/api/v1/urls')
+    .then(renderLinks)
+    .then(addLinksToPage);
+  }
+});
 
 $('.search-input').keyup(function() {
   var filter = $(this).val();
