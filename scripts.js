c00f39c3ead9146929d278e951491277e23b1701
swanie21 <kswanie21@gmail.com>
2016-11-30 08:23:58 -0700

    add function to validate url upon input

--- scripts.js
+++ scripts.js
@@ -2,6 +2,12 @@ import $ from 'jquery'
 
 $('.submit').on('click', function(e) {
   e.preventDefault();
+  const url = $('.url-component').val();
+
+  if (!validUrl(url)) {
+  return alert('Please enter a valid URL address');
+}
+
   $.post("http://localhost:3000/api/v1/urls",
     {
       url: $('.url-component').val()
@@ -24,4 +30,9 @@ function renderURL() {
 
     $("#urls").append(urlInfo);
   });
+}
+
+function validUrl(url) {
+  var urlTest = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  return urlTest.test(url);
 }
